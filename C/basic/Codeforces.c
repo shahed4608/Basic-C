aa049d5b7bf3103e90731c248eb4719fcd33c991
_jahid <zahidmuhammad095@gmail.com>
2023-08-19 21:48:10 +0600

    Codeforces

---
+++ C/basic/Codeforces.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+int g;
+scanf("%d",&g);
+printf("%d",25);
+return 0;}
---
+++ C/basic/HSC ICT/Linear_Search.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+int Roll[]={15,16,22,24,27,28,51,56,64,75};
+
+
+
+
+return 0;}
