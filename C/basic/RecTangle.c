68c95dc64c8160a3047481d8b5ab68e687d59630
shahed4608 <shahed4608@gmail.com>
2023-08-24 00:45:02 +0600

    Codeforces

---
+++ C/basic/RecTangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    float length,wide;
+    printf("\nEnter the Length of rectangle: ");
+    scanf("%f",&length);
+    printf("\nEnter the wide of rectangle: ");
+    scanf("%f",&wide);
+    printf("\nThe area of rectangle: %.2f\n",length*wide);
+    printf("The perimeter of rectangle: %.2f",2*(length+wide));
+    return 0;
+}
+
