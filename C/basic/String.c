8d2ef50f8e7bf8279f47778e333e4169b6648274
shahed4608 <shahed4608@gmail.com>
2023-09-08 22:44:12 +0600

    AUxilary

---
+++ C/basic/String.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+char str[30];
+printf("Enter The String: ");
+gets(str);
+int n = strlen(str);
+printf("\nThe String's Length is: %d\n",n);
+char aux[n],word[60];
+printf("\nEnter a string to Compare with the 1st one: ");
+gets(word);
+printf("\nThe compared value is: %d\n",strcmp(str,word));
+strcpy(aux,str);
+printf("\nThe copied string is: %s\n",aux);
+strcat(str," ");
+strcat(str,word);
+printf("\nAfter Adding another string the new string is: %s\n",str);
+return 0;
+}
+
