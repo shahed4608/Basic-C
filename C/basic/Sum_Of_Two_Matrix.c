2aa08f468e45156842376601846cfb29e76dcd7f
shahed4608 <shahed4608@gmail.com>
2023-09-19 23:16:09 +0600

    Lab Question

---
+++ C/basic/Sum_Of_Two_Matrix.c
@@ -0,0 +1,35 @@
+//write a C program to find sum of two matrices of size 3X3
+#include<stdio.h>
+int main()
+{
+    int a[3][3],b[3][3];
+    printf("Enter the first matrix below:\n");
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("\n");
+    printf("Enter the 2nd matrix below: \n");
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+    }
+    printf("\n");
+    printf("The summation of two matrix:\n");
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            printf("%d ",a[i][j]+b[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
