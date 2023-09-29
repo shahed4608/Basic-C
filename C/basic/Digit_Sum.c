3445defece08ae6ec2f6d965944ea5067081848c
shahed4608 <shahed4608@gmail.com>
2023-09-29 21:07:06 +0600

    Codeforces

---
+++ C/basic/Digit_Sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int z;
+        scanf("%d",&z);
+        z=z+1;
+        printf("%d\n",z/10);
+
+ }
+ return 0;
+}
+
